bdce956fc65b8b892ad899e1779c3a30133ca0ec
Brando Vidal <yisusdracon@gmail.com>
2022-12-02 16:56:31 -0500

    ⚒  add day01
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/day01/index.js
+++ src/day01/index.js
@@ -1,3 +1,16 @@
+const gifts = ['book', 'game', 'socks']
+
+/**
+ * It takes an array of strings and returns an array of strings
+ * @param {array} gifts - an array of strings
+ * @returns An array of strings.
+ */
 function wrapping(gifts) {
-  return []
+  return gifts.map((gift) => {
+    const WRAP = '*'.repeat(gift.length)
+    return `${WRAP}**\n*${gift}*\n**${WRAP}`
+  })
 }
+
+const wrapped = wrapping(gifts)
+console.log(wrapped)
